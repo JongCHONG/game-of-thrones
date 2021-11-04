3704bbe1577764c1b202ad2ca4ceafc0ffedaa07
JongCHONG <yonhoa27@hotmail.fr>
2021-11-04 17:18:09 +0100

    Bonus2

--- src/components/Continents.js
+++ src/components/Continents.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 
 class Continents extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      continents: []
+    }
+  } 
+
+  componentDidMount() {
+    fetch("https://thronesapi.com/api/v2/Continents")
+      .then(response => response.json()) // on transforme la donnée reçue en JSON 
+      .then(result => { this.setState({continents: result}) }) // on détaille l'action à exécuter sur ce JSON
+  }
+
   render() {
+    console.log(this.state)
+
     return (
       <div>
-        hello
+        <h2>Continents</h2>
+        <ul>
+          {this.state.continents.map((element, index) => {
+            return <li key={index}>{element.name}</li>
+          })}
+        </ul>
       </div>
     )
   }
